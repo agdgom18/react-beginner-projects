4d1fafb4baaebe6880b1a1967ec24c8ce91b86c7
Giorgi Agdgomelashvili <agdgomelashvili18@gmail.com>
2023-06-28 18:17:13 +0400

    task is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './index.scss';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button disabled={count === 0} className="minus" onClick={() => setCount(count - 1)}>
+          - Минус
+        </button>
+        <button className="plus" onClick={() => setCount(count + 1)}>
+          Плюс +
+        </button>
       </div>
     </div>
   );
